f2e9718e785de368bc2e92ddb967d64d78221bab
Ekansh Lakhyani <103509498+ekanshlakhyani@users.noreply.github.com>
2023-05-09 00:13:38 -0400

    Update main.c

--- Saturation_A02_2/main.c
+++ Saturation_A02_2/main.c
@@ -1,18 +1,3 @@
-///////////////////////////////////////////////////////////////////////////// 
-// INTEGRITY STATEMENT (v3)
-//
-// By signing your name and ID below you are stating that you have agreed
-// to the online academic integrity statement:
-// https://student.cs.uwaterloo.ca/~cs136/current/assignments/integrity.shtml
-/////////////////////////////////////////////////////////////////////////////
-// I received help from and/or collaborated with: 
- 
-// no one 
-//  
-// Name: Ekansh Lakhyani
-// login ID: elakhyan - 20945086
-///////////////////////////////////////////////////////////////////////////// 
-
 #include "cs136.h"
 
 // Use these strings in your solutions:
