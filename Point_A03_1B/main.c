b024ec06ad4119a702f12a1053c46b5fc9f1939c
Ekansh Lakhyani <103509498+ekanshlakhyani@users.noreply.github.com>
2023-05-09 00:30:14 -0400

    Update main.c

--- Point_A03_1B/main.c
+++ Point_A03_1B/main.c
@@ -1,23 +1,3 @@
-/////////////////////////////////////////////////////////////////////////////
-// INTEGRITY STATEMENT (v3)
-//
-// By signing your name and ID below you are stating that you have agreed
-// to the online academic integrity statement:
-// https://student.cs.uwaterloo.ca/~cs136/current/assignments/integrity.shtml
-/////////////////////////////////////////////////////////////////////////////
-// I received help from and/or collaborated with:
-
-// no one
-//
-// Name: Ekansh Lakhyani
-// login ID: elakhyan - 20945086
-/////////////////////////////////////////////////////////////////////////////
-
-#include "cs136.h"
-
-// By including this line, you are now able to use all
-//  functions from q1a-rtnl in this question
-//  You will learn more about the role of .h files in Section 6
 #include "q1a-rtnl.h"
 
 
