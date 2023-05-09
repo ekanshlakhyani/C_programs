513b7b03ec04e477fb1753068759c1810141bffd
Ekansh Lakhyani <103509498+ekanshlakhyani@users.noreply.github.com>
2023-05-09 16:40:04 -0400

    Update balanced1.c

--- Unbalance_A04_4A/balanced1.c
+++ Unbalance_A04_4A/balanced1.c
@@ -1,31 +1,3 @@
-/////////////////////////////////////////////////////////////////////////////
-// INTEGRITY INSTRUCTIONS (v2)
-
-// Explicitly state the level of collaboration on this question
-// Examples:
-//   * I discussed ideas with classmate(s) [include name(s)]
-//   * I worked together with classmate(s) in the lab [include name(s)]
-//   * Classmate [include name] helped me debug my code
-//   * I consulted website [include url]
-//   * None
-//
-// A "None" indicates you completed this question entirely by yourself
-// (or with assistance from course staff, which you do not have to mention)
-/////////////////////////////////////////////////////////////////////////////
-// INTEGRITY STATEMENT:
-// I received help from and/or collaborated with:
-
-// none
-
-// Name: Ekansh Lakhyani 
-// login ID: elakhyan - 20945086
-/////////////////////////////////////////////////////////////////////////////
-
-#include "cs136-trace.h"
-#include <stdio.h>
-
-// is_balance reads input and prints balanced if the brackets are balanced.
-// effects: reads input and prints to the screen.
 void is_balance() {
   char c;
   int open_no = 0;
